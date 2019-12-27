00da1c3ac1c6cd06748608908720abed87aeea71
Gus Smith <guscomps@gmail.com>
2019-12-27 13:31:29 -0800

    Clean up utility file

--- tests/run_models.py
+++ tests/run_models.py
@@ -17,18 +17,9 @@
 """Utilities for running models with different datatypes."""
 
 import tvm
-import topi.testing
 import numpy as np
 from tvm import relay
-from tvm.relay.testing.inception_v3 import get_workload as get_inception
-from tvm.relay.testing.resnet import get_workload as get_resnet
-from tvm.relay.testing.mobilenet import get_workload as get_mobilenet
-from nose.tools import nottest
-from load_datatypes import load_bfloat, load_posit8, load_posit16, load_posit32
 from util import change_dtype, convert_ndarray
-
-tgt = "llvm"
-
 
 
 def run_model(get_workload,
