d4a99e04007456447003fd1ee828ee82d64c0217
Gus Smith <guscomps@gmail.com>
2019-12-30 16:17:04 -0800

    Remove erroneous $s (and a /)

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ if [ -z "$basedir" ]; then
 fi
 
 # A format similar to ISO 8601, in UTC
-logdir="$basedir/$(TZ=UTC date +%Y-%m-%d_%H-%M-%SZ)/"
+logdir="$basedir/$(TZ=UTC date +%Y-%m-%d_%H-%M-%SZ)"
 mkdir -p $logdir
 
-python3 ./tests/test-basic.py > $"$logdir/test-basic.log"
-python3 ./tests/test1.py > $"$logdir/test1.log"
+python3 ./tests/test-basic.py > "$logdir/test-basic.log"
+python3 ./tests/test1.py > "$logdir/test1.log"
