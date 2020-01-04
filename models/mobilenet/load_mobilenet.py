2fc73236d8e8bb611587360ef16f04d0fdb7fe54
Gus Smith <guscomps@gmail.com>
2020-01-03 17:45:56 -0800

    Finish loading mobilenet

--- models/mobilenet/load_mobilenet.py
+++ models/mobilenet/load_mobilenet.py
@@ -23,7 +23,8 @@ def get_tf_param_name(relay_param_name):
         return 'MobilenetV1/Logits/Conv2d_1c_1x1/weights'
 
     import re
-    match = re.match('^separable_conv_block_(?P<layernum>\d+)_(?P<op>.*)$', relay_param_name)
+    match = re.match('^separable_conv_block_(?P<layernum>\d+)_(?P<op>.*)$',
+                     relay_param_name)
     if not match:
         raise ValueError(
             'Unexpected variable name: {}'.format(relay_param_name))
@@ -61,22 +62,39 @@ def get_tf_param_name(relay_param_name):
         depth_or_point = 'pointwise'
         op = 'BatchNorm/beta'
     else:
-        raise ValueError(
-            'Unexpected or None value for op: {}'.format(match.group('op')))
+        raise ValueError('Unexpected or None value for op: {}'.format(
+            match.group('op')))
 
     return 'MobilenetV1/Conv2d_{}_{}/{}'.format(layer_num, depth_or_point, op)
 
 
-module, params = get_mobilenet()
+# Make sure to set the layout and image shape to match TF style
+module, params = get_mobilenet(layout='NHWC',
+                               image_shape=(224, 224, 3),
+                               num_classes=1001)
 module = tvm.relay.transform.SimplifyInference()(module)
 module = tvm.relay.transform.DeadCodeElimination()(module)
 free_vars = tvm.relay.analysis.free_vars(module['main'].body)
 free_vars = free_vars[1:]
 
-reader = py_checkpoint_reader.NewCheckpointReader('./mobilenet_v1_1.0_224.ckpt')
+reader = py_checkpoint_reader.NewCheckpointReader(
+    './mobilenet_v1_1.0_224.ckpt')
 
-for free_var in free_vars:
-    #print(free_var.name_hint)
-    name = get_tf_param_name(free_var.name_hint)
-    print(reader.get_tensor(name))
+# Maps strings to NDArrays, like the old `params` object, but the NDArrays are
+# the trained parameters.
+new_params = {}
+for name, var in params.items():
+    shape = tuple(map(int, var.shape))
+    tf_tensor_name = get_tf_param_name(name)
+    tf_tensor = reader.get_tensor(tf_tensor_name)
 
+    # Hard-coding this transformation.
+    if name == 'fc_weight':
+        tf_tensor = tf_tensor.squeeze().swapaxes(0, 1)
+
+    tf_tensor_shape = tf_tensor.shape
+
+    assert shape == tf_tensor_shape, "Shapes for variable %s do not match: %s vs. %s" % (
+        name, shape, tf_tensor_shape)
+
+    new_params[name] = tf_tensor
