4c07018de66ce162bdb7f74caa5a4fd77b783124
Gus Smith <guscomps@gmail.com>
2020-01-16 11:39:39 -0800

    BUGFIX: Load posit32 manually, and convert input image to float32
    This makes inference with pretrained weights actually work! But it works slowly.

--- tests/test-cifar10-mobilenet-posit32.py
+++ tests/test-cifar10-mobilenet-posit32.py
@@ -6,6 +6,8 @@ import numpy as np
 from sys import stderr
 from util.load_datatypes import load_posit32
 from util.change_dtype import change_dtype, convert_ndarray
+from ctypes import CDLL, RTLD_GLOBAL
+from os import path
 
 # Copied from https://github.com/kuangliu/pytorch-cifar/blob/ab908327d44bf9b1d22cd333a4466e85083d3f21/main.py#L36
 transform = torchvision.transforms.Compose([
@@ -22,8 +24,49 @@ dataset = torchvision.datasets.CIFAR10('.',
 
 module, params, image_shape = load_mobilenet()
 
+# Register datatype manually
+CDLL(
+    path.join(path.abspath(path.dirname(__file__)),
+              '../datatypes/universal-wrapper/universal-wrapper.so'),
+    RTLD_GLOBAL)
+tvm.datatype.register("posit32", 131)
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_FloatToPosit32es2"),
+                         "Cast", "llvm", "posit32", "float")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2ToFloat"),
+                         "Cast", "llvm", "float", "posit32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_IntToPosit32es2"),
+                         "Cast", "llvm", "posit32", "int")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2Add"),
+                         "Add", "llvm", "posit32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2Sub"),
+                         "Sub", "llvm", "posit32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_FloatToPosit32es2"),
+                         "FloatImm", "llvm", "posit32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2Mul"),
+                         "Mul", "llvm", "posit32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2Div"),
+                         "Div", "llvm", "posit32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2Max"),
+                         "Max", "llvm", "posit32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2Sqrt"),
+                         "Call",
+                         "llvm",
+                         "posit32",
+                         intrinsic_name="sqrt")
+tvm.datatype.register_op(tvm.datatype.lower_ite,
+                         "Call",
+                         "llvm",
+                         "posit32",
+                         intrinsic_name="tvm_if_then_else")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit32es2Exp"),
+                         "Call",
+                         "llvm",
+                         "posit32",
+                         intrinsic_name="exp")
+# TODO(gus) these aren't actually right. these are double min(actually lowest)/max.
+tvm.datatype.register_min_func(lambda num_bits: -1.79769e+308, "posit32")
+
 # Change the datatype
-load_posit32()
 conversion_executor = relay.create_executor()
 expr, params = change_dtype('float32', 'custom[posit32]32', module['main'],
                             params, conversion_executor)
@@ -36,7 +79,7 @@ tested = 0
 correct = 0
 for image, target_class in dataset:
     # Add batch dimension
-    image_tvm = np.expand_dims(image.numpy(), axis=0)
+    image_tvm = np.expand_dims(image.numpy().astype('float32'), axis=0)
 
     # Change datatype of input
     image_tvm = convert_ndarray('custom[posit32]32', image_tvm, conversion_executor)
