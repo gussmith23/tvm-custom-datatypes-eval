8a332c449ea6499b27344afe7ebac17f178e6b73
Gus Smith <guscomps@gmail.com>
2020-01-16 13:49:17 -0800

    Correct posit8 test and insert correct min

--- tests/test-cifar1-mobilenet-posit8.py
+++ tests/test-cifar1-mobilenet-posit8.py
@@ -4,8 +4,9 @@ import torchvision
 from models.mobilenet_pytorch.load import load_mobilenet
 import numpy as np
 from sys import stderr
-from util.load_datatypes import load_posit8
 from util.change_dtype import change_dtype, convert_ndarray
+from ctypes import CDLL, RTLD_GLOBAL
+from os import path
 
 # Copied from https://github.com/kuangliu/pytorch-cifar/blob/ab908327d44bf9b1d22cd333a4466e85083d3f21/main.py#L36
 transform = torchvision.transforms.Compose([
@@ -22,8 +23,50 @@ dataset = torchvision.datasets.CIFAR10('.',
 
 module, params, image_shape = load_mobilenet()
 
+# Register datatype manually
+CDLL(
+    path.join(path.abspath(path.dirname(__file__)),
+              '../datatypes/universal-wrapper/universal-wrapper.so'),
+    RTLD_GLOBAL)
+tvm.datatype.register("posit8", 131)
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_FloatToPosit8es0"),
+                         "Cast", "llvm", "posit8", "float")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0ToFloat"),
+                         "Cast", "llvm", "float", "posit8")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_IntToPosit8es0"),
+                         "Cast", "llvm", "posit8", "int")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0Add"),
+                         "Add", "llvm", "posit8")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0Sub"),
+                         "Sub", "llvm", "posit8")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_FloatToPosit8es0"),
+                         "FloatImm", "llvm", "posit8")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0Mul"),
+                         "Mul", "llvm", "posit8")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0Div"),
+                         "Div", "llvm", "posit8")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0Max"),
+                         "Max", "llvm", "posit8")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0Sqrt"),
+                         "Call",
+                         "llvm",
+                         "posit8",
+                         intrinsic_name="sqrt")
+tvm.datatype.register_op(tvm.datatype.lower_ite,
+                         "Call",
+                         "llvm",
+                         "posit8",
+                         intrinsic_name="tvm_if_then_else")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit8es0Exp"),
+                         "Call",
+                         "llvm",
+                         "posit8",
+                         intrinsic_name="exp")
+# es = 0, useed = 2. first bit is sign bit, then 7 bits of '1' for the regime
+# gives us a k value of 6. Then our number is 2**6.
+tvm.datatype.register_min_func(lambda num_bits: -64, "posit8")
+
 # Change the datatype
-load_posit8()
 conversion_executor = relay.create_executor()
 expr, params = change_dtype('float32', 'custom[posit8]8', module['main'],
                             params, conversion_executor)
@@ -36,10 +79,11 @@ tested = 0
 correct = 0
 for image, target_class in dataset:
     # Add batch dimension
-    image_tvm = np.expand_dims(image.numpy(), axis=0)
+    image_tvm = np.expand_dims(image.numpy().astype('float32'), axis=0)
 
     # Change datatype of input
-    image_tvm = convert_ndarray('custom[posit8]8', image_tvm, conversion_executor)
+    image_tvm = convert_ndarray('custom[posit8]8', image_tvm,
+                                conversion_executor)
 
     with tvm.build_config(disable_vectorize=True):
         output = mobilenet(image_tvm, **params)
