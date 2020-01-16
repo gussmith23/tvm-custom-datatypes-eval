a54b52ccff29f8524daaf4b2c153238a2e3acd93
Gus Smith <guscomps@gmail.com>
2020-01-16 15:20:02 -0800

    Outputs rows of CSV, including time

--- tests/util/run_pretrained_model_test.py
+++ tests/util/run_pretrained_model_test.py
@@ -3,12 +3,16 @@ import numpy as np
 import tvm
 from tvm import relay
 from .change_dtype import change_dtype, convert_ndarray
+from time import perf_counter_ns
 
 
 def run_pretrained_model_test(module, params, dataset, dtype):
     conversion_executor = relay.create_executor()
     ex = relay.create_executor(mod=module)
     model = ex.evaluate()
+
+    columns = ['inference time (ns)', 'output class number', 'expected class number', 'output correct?']
+    print(','.join(columns))
 
     tested = 0
     correct = 0
@@ -20,7 +24,9 @@ def run_pretrained_model_test(module, params, dataset, dtype):
         image_tvm = convert_ndarray(dtype, image_tvm, conversion_executor)
 
         with tvm.build_config(disable_vectorize=True):
+            start = perf_counter_ns()
             output = model(image_tvm, **params)
+            elapsed = perf_counter_ns() - start
             output = output.data
 
         # convert output
@@ -33,6 +39,14 @@ def run_pretrained_model_test(module, params, dataset, dtype):
         print('{} of {} correct ({})'.format(correct, tested,
                                              correct / tested),
               file=stderr)
+
+        data = {
+            'inference time (ns)': elapsed,
+            'output class number': argmax_tvm,
+            'expected class number': target_class,
+            'output correct?': int(argmax_tvm == target_class)
+        }
+        print(','.join([str(data[column]) for column in columns]))
 
     print('Model accuracy:')
     print(correct / tested)
