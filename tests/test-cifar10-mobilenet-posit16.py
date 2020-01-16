2ce33967174660f95dd18fdd7ded0c70f54acd8e
Gus Smith <guscomps@gmail.com>
2020-01-16 13:18:46 -0800

    Apply fixes for posit16

--- tests/test-cifar10-mobilenet-posit16.py
+++ tests/test-cifar10-mobilenet-posit16.py
@@ -4,8 +4,9 @@ import torchvision
 from models.mobilenet_pytorch.load import load_mobilenet
 import numpy as np
 from sys import stderr
-from util.load_datatypes import load_posit16
 from util.change_dtype import change_dtype, convert_ndarray
+from ctypes import CDLL, RTLD_GLOBAL
+from os import path
 
 # Copied from https://github.com/kuangliu/pytorch-cifar/blob/ab908327d44bf9b1d22cd333a4466e85083d3f21/main.py#L36
 transform = torchvision.transforms.Compose([
@@ -22,8 +23,49 @@ dataset = torchvision.datasets.CIFAR10('.',
 
 module, params, image_shape = load_mobilenet()
 
+# Register datatype manually
+CDLL(
+    path.join(path.abspath(path.dirname(__file__)),
+              '../datatypes/universal-wrapper/universal-wrapper.so'),
+    RTLD_GLOBAL)
+tvm.datatype.register("posit16", 131)
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_FloatToPosit16es1"),
+                         "Cast", "llvm", "posit16", "float")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1ToFloat"),
+                         "Cast", "llvm", "float", "posit16")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_IntToPosit16es1"),
+                         "Cast", "llvm", "posit16", "int")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1Add"),
+                         "Add", "llvm", "posit16")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1Sub"),
+                         "Sub", "llvm", "posit16")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_FloatToPosit16es1"),
+                         "FloatImm", "llvm", "posit16")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1Mul"),
+                         "Mul", "llvm", "posit16")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1Div"),
+                         "Div", "llvm", "posit16")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1Max"),
+                         "Max", "llvm", "posit16")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1Sqrt"),
+                         "Call",
+                         "llvm",
+                         "posit16",
+                         intrinsic_name="sqrt")
+tvm.datatype.register_op(tvm.datatype.lower_ite,
+                         "Call",
+                         "llvm",
+                         "posit16",
+                         intrinsic_name="tvm_if_then_else")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("_Posit16es1Exp"),
+                         "Call",
+                         "llvm",
+                         "posit16",
+                         intrinsic_name="exp")
+# TODO(gus) these aren't actually right. these are double min(actually lowest)/max.
+tvm.datatype.register_min_func(lambda num_bits: -1.79769e+308, "posit16")
+
 # Change the datatype
-load_posit16()
 conversion_executor = relay.create_executor()
 expr, params = change_dtype('float32', 'custom[posit16]16', module['main'],
                             params, conversion_executor)
@@ -36,10 +78,11 @@ tested = 0
 correct = 0
 for image, target_class in dataset:
     # Add batch dimension
-    image_tvm = np.expand_dims(image.numpy(), axis=0)
+    image_tvm = np.expand_dims(image.numpy().astype('float32'), axis=0)
 
     # Change datatype of input
-    image_tvm = convert_ndarray('custom[posit16]16', image_tvm, conversion_executor)
+    image_tvm = convert_ndarray('custom[posit16]16', image_tvm,
+                                conversion_executor)
 
     with tvm.build_config(disable_vectorize=True):
         output = mobilenet(image_tvm, **params)
