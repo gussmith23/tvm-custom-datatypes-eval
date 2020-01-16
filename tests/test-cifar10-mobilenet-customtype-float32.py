9eda2cb90e2899ecb7dfafbb9fed643f493ba903
Gus Smith <guscomps@gmail.com>
2020-01-16 13:58:21 -0800

    Fix ourfloat32

--- tests/test-cifar10-mobilenet-customtype-float32.py
+++ tests/test-cifar10-mobilenet-customtype-float32.py
@@ -4,8 +4,9 @@ import torchvision
 from models.mobilenet_pytorch.load import load_mobilenet
 import numpy as np
 from sys import stderr
-from util.load_datatypes import load_float32
 from util.change_dtype import change_dtype, convert_ndarray
+from ctypes import CDLL, RTLD_GLOBAL
+from os import path
 
 # Copied from https://github.com/kuangliu/pytorch-cifar/blob/ab908327d44bf9b1d22cd333a4466e85083d3f21/main.py#L36
 transform = torchvision.transforms.Compose([
@@ -22,10 +23,51 @@ dataset = torchvision.datasets.CIFAR10('.',
 
 module, params, image_shape = load_mobilenet()
 
+# Register datatype manually
+CDLL(
+    path.join(path.abspath(path.dirname(__file__)),
+              '../datatypes/float32/float32.so'), RTLD_GLOBAL)
+tvm.datatype.register("ourfloat32", 131)
+tvm.datatype.register_op(tvm.datatype.create_lower_func("FloatToOurFloat32"),
+                         "Cast", "llvm", "ourfloat32", "float")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32ToFloat"),
+                         "Cast", "llvm", "float", "ourfloat32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("IntToOurFloat32"),
+                         "Cast", "llvm", "ourfloat32", "int")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32Add"),
+                         "Add", "llvm", "ourfloat32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32Sub"),
+                         "Sub", "llvm", "ourfloat32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("FloatToOurFloat32"),
+                         "FloatImm", "llvm", "ourfloat32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32Mul"),
+                         "Mul", "llvm", "ourfloat32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32Div"),
+                         "Div", "llvm", "ourfloat32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32Max"),
+                         "Max", "llvm", "ourfloat32")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32Sqrt"),
+                         "Call",
+                         "llvm",
+                         "ourfloat32",
+                         intrinsic_name="sqrt")
+tvm.datatype.register_op(tvm.datatype.lower_ite,
+                         "Call",
+                         "llvm",
+                         "ourfloat32",
+                         intrinsic_name="tvm_if_then_else")
+tvm.datatype.register_op(tvm.datatype.create_lower_func("OurFloat32Exp"),
+                         "Call",
+                         "llvm",
+                         "ourfloat32",
+                         intrinsic_name="exp")
+# es = 0, useed = 2. first bit is sign bit, then 7 bits of '1' for the regime
+# gives us a k value of 6. Then our number is 2**6.
+tvm.datatype.register_min_func(lambda num_bits: -3.4028235e+38, "ourfloat32")
+
 # Change the datatype
-load_float32()
 conversion_executor = relay.create_executor()
-expr, params = change_dtype('float32', 'custom[float32]32', module['main'],
+expr, params = change_dtype('float32', 'custom[ourfloat32]32', module['main'],
                             params, conversion_executor)
 module = relay.module.Module.from_expr(expr)
 
@@ -36,10 +78,11 @@ tested = 0
 correct = 0
 for image, target_class in dataset:
     # Add batch dimension
-    image_tvm = np.expand_dims(image.numpy(), axis=0)
+    image_tvm = np.expand_dims(image.numpy().astype('float32'), axis=0)
 
     # Change datatype of input
-    image_tvm = convert_ndarray('custom[float32]32', image_tvm, conversion_executor)
+    image_tvm = convert_ndarray('custom[ourfloat32]32', image_tvm,
+                                conversion_executor)
 
     with tvm.build_config(disable_vectorize=True):
         output = mobilenet(image_tvm, **params)
